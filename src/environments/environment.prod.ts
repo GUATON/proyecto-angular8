b62ec642070c2b0d9e4ddb9ad49ca844571849ee
Marcelo Toro <marcelo.toro@upv.cl>
2021-03-21 18:45:48 -0300

    ultimos cambios en url y front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBLpA0FbYXC_x5N64HuYg1OxIqPmIugTAc",
+    authDomain: "proyecto-bbb19.firebaseapp.com",
+    databaseURL: "https://proyecto-bbb19-default-rtdb.firebaseio.com",
+    projectId: "proyecto-bbb19",
+    storageBucket: "proyecto-bbb19.appspot.com",
+    messagingSenderId: "649651865753",
+    appId: "1:649651865753:web:426dc6900f8c3d56bc5008"
+  }
 };
